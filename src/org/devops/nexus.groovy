b684571f85d41fa29403795fd8bfdbe235380459
DuJingFeng <56363329+e1evenDu@users.noreply.github.com>
2020-09-22 20:48:06 +0700

    Create nexus.groovy

---
+++ src/org/devops/nexus.groovy
@@ -0,0 +1,48 @@
+pakcage org.devops
+
+
+//获取POM中的坐标
+def GetGav(){
+   //上传制品
+    def jarName = sh returnStdout: true, script: "cd target;ls *.jar"
+    env.jarName = jarName - "\n"
+    
+    def pom = readMavenPom file: 'pom.xml'
+    env.pomVersion = "${pom.version}"
+    env.pomArtifact = "${pom.artifactId}"
+    env.pomPackaging = "${pom.packaging}"
+    env.pomGroupId = "${pom.groupId}"
+    
+    println("${pomGroupId}-${pomArtifact}-${pomVersion}-${pomPackaging}")
+
+    return ["${pomGroupId}","${pomArtifact}","${pomVersion}","${pomPackaging}"]
+}
+
+//Nexus plugin deploy
+def NexusUpload(){
+    //use nexus plugin
+    nexusArtifactUploader artifacts: [[artifactId: "${pomArtifact}", 
+                                        classifier: '', 
+                                        file: "${filePath}", 
+                                        type: "${pomPackaging}"]], 
+                            credentialsId: 'nexus-admin-userpwd', 
+                            groupId: "${pomGroupId}", 
+                            nexusUrl: '10.0.0.10:808', 
+                            nexusVersion: 'nexus3', 
+                            protocol: 'http', 
+                            repository: "${repoName}", 
+                            version: "${pomVersion}"
+}
+
+//mvn deploy
+def MavenUpload(){          
+    def mvnHome = tool "M2"
+    sh  """ 
+        cd target/
+        ${mvnHome}/bin/mvn deploy:deploy-file -Dmaven.test.skip=true  \
+                                -Dfile=${jarName} -DgroupId=${pomGroupId} \
+                                -DartifactId=${pomArtifact} -Dversion=${pomVersion}  \
+                                -Dpackaging=${pomPackaging} -DrepositoryId=maven-hostd \
+                                -Durl=http://10.0.0.10:808/repository/maven-hostd 
+        """
+}
