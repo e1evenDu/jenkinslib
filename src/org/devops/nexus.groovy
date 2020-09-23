1b6def63d9d02a2b291074db98dfc2d217ff6be6
DuJingFeng <56363329+e1evenDu@users.noreply.github.com>
2020-09-23 19:22:46 +0700

    Update nexus.groovy

--- src/org/devops/nexus.groovy
+++ src/org/devops/nexus.groovy
@@ -47,6 +47,42 @@ def MavenUpload(){
         """
 }
 
+//制品晋级
+def ArtifactUpdate(updateType,artifactUrl){
+
+    //晋级策略
+    if ("${updateType}" == "snapshot -> release"){
+        println("snapshot -> release")
+
+        //下载原始制品
+        sh "  rm -fr updates && mkdir updates && cd updates && wget ${artifactUrl} && ls -l "
+
+        //获取artifactID 
+        
+        artifactUrl = artifactUrl -  "http://10.0.0.10:8081/repository/maven-hostd/"
+        artifactUrl = artifactUrl.split("/").toList()
+        
+        println(artifactUrl.size())
+        env.jarName = artifactUrl[-1] 
+        env.pomVersion = artifactUrl[-2].replace("SNAPSHOT","RELEASE")
+        env.pomArtifact = artifactUrl[-3]
+        pomPackaging = artifactUrl[-1]
+        pomPackaging = pomPackaging.split("\\.").toList()[-1]
+        env.pomPackaging = pomPackaging[-1]
+        env.pomGroupId = artifactUrl[0..-4].join(".")
+        println("${pomGroupId}##${pomArtifact}##${pomVersion}##${pomPackaging}")
+        env.newJarName = "${pomArtifact}-${pomVersion}.${pomPackaging}"
+        
+        //更改名称
+        sh " cd updates && mv ${jarName} ${newJarName} "
+        
+        //上传制品
+        env.repoName = "maven-release"
+        env.filePath = "updates/${newJarName}"
+        NexusUpload()
+    }
+}
+
 def main(uploadType){
     GetGav()
     if ("${uploadType}" == "maven"){
